c8e1d39a4bce977ec44102191792c188c84d4224
AnhMap <nguyenlephong1997@gmail.com>
2019-01-28 06:54:16 +0700

    first build

---
+++ precache-manifest.495e82e021db1d5c396ee300461c15b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4fe2579df54b9d6ec21f",
+    "url": "/static/js/main.4fe2579d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4fe2579df54b9d6ec21f",
+    "url": "/static/css/main.904f16e6.chunk.css"
+  },
+  {
+    "revision": "e4ceb5810ad545a65f2e837a97323214",
+    "url": "/index.html"
+  }
+];
