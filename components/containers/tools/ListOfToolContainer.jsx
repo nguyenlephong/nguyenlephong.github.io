9f4819f381fc8938bf0b02e8d55883e3780e8dd5
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2021-11-24 15:50:58 +0700

    Develop tool page

--- components/containers/tools/ListOfToolContainer.jsx
+++ components/containers/tools/ListOfToolContainer.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Box, Grid } from "@mui/material";
+import { tools } from "../../../lib/tools";
 
 const ListOfToolContainer = props => {
   return (
     <Box sx={{padding: 2, width: "100%", height: "100%"}}>
       <Grid container spacing={2}>
-      {[1,2,3,4,5].map(tool => {
+      {tools.map(tool => {
         return (
           <Grid key={tool?.id} item xs={12} md={6} lg={4}>
-            <ToolCardItem />
+            <ToolCardItem data={tool} />
           </Grid>
         )
       })}
@@ -26,10 +27,11 @@ export default ListOfToolContainer;
 
 
 export const ToolCardItem = (props) => {
+  const {data} = props;
   return (
     <div className={"tool-card-item"}>
-      <div>name</div>
-      <div>description</div>
+      <div className={"title"}>{data?.name}</div>
+      <div className={"description"}>{data?.description}</div>
     </div>
   )
 }
