2c529db74c5551f98cb7a544f425b0215e018d05
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2021-11-30 20:11:03 +0700

    Update wrapper component for tool detail page

--- pages/tools/search-param-url.js
+++ pages/tools/search-param-url.js
@@ -1,36 +1,16 @@
 import React  from "react";
-import Head from "next/head";
-import Header from "../../components/components/Header";
-import { Fade } from "react-bootstrap";
-import Footer from "../../components/components/Footer";
-import TopButton from "../../components/components/TopButton";
-import WrapperProvider from "../../components/WrapperProvider";
-import { useSelector } from "reduxs/store";
 import SearchParamURLPage from "../../components/containers/tools/tool_009/SearchParamURLPage";
+import ToolDetailPageWrapper from "../../components/containers/tools/ToolDetailPageWrapper";
 
 const Tool009 = props => {
-  const chosenThemeInit = useSelector(store => store.themes).currentThemes;
-
   return (
-    <WrapperProvider>
-      <Head>
-        <title>{ "Tools For Developer | Get all param from url to JSON object"}</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Tools For Developer. A passionate individual who always thrives to work on end-to-end products which develop sustainable and scalable social and technical systems to create impact."
-        />
-      </Head>
-      <div>
-        <Header theme={chosenThemeInit} />
-        <Fade bottom duration={2000} distance="40px">
-          <SearchParamURLPage/>
-        </Fade>
-        <Footer theme={chosenThemeInit} />
-        <TopButton theme={chosenThemeInit} />
-      </div>
-    </WrapperProvider>
-  );
+    <ToolDetailPageWrapper
+      title={"Tools For Developer | Get all param from url to JSON object"}
+      description={"Tools For Developer | Get all param from url to JSON object"}
+    >
+      <SearchParamURLPage/>
+    </ToolDetailPageWrapper>
+  )
 };
 
 Tool009.propTypes = {
