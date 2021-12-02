7cbcaf2eab48923141740e3900fec7a16e7502b9
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2021-12-02 09:40:17 +0700

    Update options for monaco editor

--- components/containers/tools/ide-monaco/MonacoConfig.js
+++ components/containers/tools/ide-monaco/MonacoConfig.js
@@ -14,6 +14,7 @@ export const IDEMonacoOptions = {
   disableMonospaceOptimizations: false,
   dragAndDrop: false,
   fixedOverflowWidgets: false,
+  lineNumbers: "off",
   folding: true,
   foldingStrategy: 'auto',
   fontLigatures: false,
