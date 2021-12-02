ad732b978e66451f441c885388c251bbeb46d665
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2021-12-02 22:28:57 +0700

    Add react markdown editor online

--- next.config.js
+++ next.config.js
@@ -2,46 +2,52 @@
  * Next Config
  */
 const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')([]);
+const removeImports = require('next-remove-imports')();
 
-module.exports = withPlugins([
-  {
-    // basePath: '/',
-    // assetPrefix: '/',
-    webpack5: true,
-    distDir: 'build',
-    reactStrictMode: false,
-    devIndicators: {
-      autoPrerender: false,
-    },
-    images: {
-      disableStaticImages: true,
-      domain: "https://nguyenlephong.github.io/",
-      // path: `/_next/image`,
-    },
-    eslint: {
-      dirs: [
-        'components',
-        'lib',
-        'pages'
-      ]
-    },
-    webpack: (config, { dev, isServer }) => {
-      // Replace React with Preact only in client production build
-      if (!dev && !isServer) {
-        config.resolve.fallback = {
-          "net": false,
-          "fs": false,
-          "tls": false,
-          "path": false,
-          "crypto": false,
-          "http": false,
-          "stream": false,
-          "https": false,
-          "zlib": false,
-        }
-      }
-      return config
-    }
+
+module.exports = withPlugins([withTM], removeImports({
+  // basePath: '/',
+  // assetPrefix: '/',
+  webpack5: true,
+  distDir: 'build',
+  reactStrictMode: false,
+  devIndicators: {
+    autoPrerender: false,
   },
-]);
+  images: {
+    disableStaticImages: true,
+    domain: "https://nguyenlephong.github.io/",
+    // path: `/_next/image`,
+  },
+  eslint: {
+    dirs: [
+      'components',
+      'lib',
+      'pages'
+    ]
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      config.resolve.fallback = {
+        "net": false,
+        "fs": false,
+        "tls": false,
+        "path": false,
+        "crypto": false,
+        "http": false,
+        "stream": false,
+        "https": false,
+        "zlib": false,
+      }
+    }
 
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+
+    return config
+  }
+}));
