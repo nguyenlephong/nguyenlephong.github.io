9a971de77f951ed416e7adb179466535b583f0f0
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2021-12-14 22:27:12 +0700

    Update UI AboutPage

--- components/containers/about/AboutPage.js
+++ components/containers/about/AboutPage.js
@@ -1,27 +1,30 @@
 import React, { useCallback, useState } from "react";
-import Gallery from "react-photo-gallery";
+// import Gallery from "react-photo-gallery";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import { photos, summaries, videos } from "../../../lib/about-data";
 import ListOfVideosComponent from "./ListOfVideosComponent";
 import ProfileSummaryComponent from "./ProfileSummaryComponent";
+import { ImageList, ImageListItem } from "@mui/material";
+import { useWindowSize } from "react-use";
 
 const AboutPage = (props) => {
+  const {width} = useWindowSize()
   const theme = props.theme;
 
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  // const [currentImage, setCurrentImage] = useState(0);
+  // const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
+  // const openLightbox = useCallback((event, { photo, index }) => {
+  //   setCurrentImage(index);
+  //   setViewerIsOpen(true);
+  // }, []);
+  //
+  // const closeLightbox = () => {
+  //   setCurrentImage(0);
+  //   setViewerIsOpen(false);
+  // };
 
   return (
     <div className="contact-main about-header">
@@ -40,28 +43,41 @@ const AboutPage = (props) => {
           My Gallery
         </h1>
 
-        <Gallery
-          targetRowHeight={420}
-          columns={4}
-          direction="row"
-          photos={photos}
-          onClick={openLightbox}
-        />
-
-        <ModalGateway>
-          {viewerIsOpen ? (
-            <Modal onClose={closeLightbox}>
-              <Carousel
-                currentIndex={currentImage}
-                views={photos.map((x) => ({
-                  ...x,
-                  srcset: x.srcSet,
-                  caption: x.title,
-                }))}
+        <ImageList variant="masonry" cols={width > 1368 ? 3 : width < 768 ? 1 : 2} gap={12}>
+          {photos.map((item) => (
+            <ImageListItem key={item.src} sx={{borderRadius: 8}}>
+              <img
+                src={`${item.src}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.src}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.alt}
+                loading="lazy"
               />
-            </Modal>
-          ) : null}
-        </ModalGateway>
+            </ImageListItem>
+          ))}
+        </ImageList>
+
+        {/*<Gallery*/}
+        {/*  targetRowHeight={420}*/}
+        {/*  columns={4}*/}
+        {/*  direction="row"*/}
+        {/*  photos={photos}*/}
+        {/*  onClick={openLightbox}*/}
+        {/*/>*/}
+
+        {/*<ModalGateway>*/}
+        {/*  {viewerIsOpen ? (*/}
+        {/*    <Modal onClose={closeLightbox}>*/}
+        {/*      <Carousel*/}
+        {/*        currentIndex={currentImage}*/}
+        {/*        views={photos.map((x) => ({*/}
+        {/*          ...x,*/}
+        {/*          srcset: x.srcSet,*/}
+        {/*          caption: x.title,*/}
+        {/*        }))}*/}
+        {/*      />*/}
+        {/*    </Modal>*/}
+        {/*  ) : null}*/}
+        {/*</ModalGateway>*/}
 
         <h1 className="skills-header" style={{ color: theme.text }}>
           My Videos
