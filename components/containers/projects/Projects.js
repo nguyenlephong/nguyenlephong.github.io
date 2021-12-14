acf482e5bdde5ff487411d71b6cffc25c9026779
Nguyen Le Phong <nguyenlephong1997@gmail.com>
2021-12-14 22:11:14 +0700

    Update UI project page

--- components/containers/projects/Projects.js
+++ components/containers/projects/Projects.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import GithubRepoCard from "../../components/GithubRepoCard";
+// import GithubRepoCard from "../../components/GithubRepoCard";
 import Button from "../../components/Button";
 import { Fade } from "react-reveal";
 import { projectsHeader } from "../../../lib/portfolio.js";
 import ProjectsData from "../../../src/shared/opensource/projects.json";
 import ProjectsImg from "./ProjectsImg";
+import { Box, Grid, Typography } from "@mui/material";
+import ProjectCard from "../../components/ProjectCard";
+import Masonry from '@mui/lab/Masonry';
 
 class Projects extends Component {
   render() {
@@ -14,34 +17,57 @@ class Projects extends Component {
     return (
       <div className="projects-main">
         <Header theme={theme} />
-        <div className="basic-projects">
+        <Box className="basic-projects">
           <Fade bottom duration={2000} distance="40px">
-            <div className="projects-heading-div">
-              <div className="projects-heading-img-div">
+            <Box className="projects-heading-div">
+              <Box className="projects-heading-img-div">
                 <ProjectsImg theme={theme} />
-              </div>
-              <div className="projects-heading-text-div">
+              </Box>
+              <Box className="projects-heading-text-div">
                 <h1
                   className="projects-heading-text"
                   style={{ color: theme.text }}
                 >
                   {projectsHeader.title}
                 </h1>
-                <p
+                <Typography
+
                   className="projects-header-detail-text subTitle"
-                  style={{ color: theme.secondaryText }}
+                  sx={{ color: theme.secondaryText, pt: 2 }}
                 >
                   {projectsHeader["description"]}
-                </p>
-              </div>
-            </div>
+                </Typography>
+              </Box>
+            </Box>
           </Fade>
-        </div>
-        <div className="repo-cards-div-main">
-          {ProjectsData.data.map((repo) => {
-            return <GithubRepoCard repo={repo} theme={theme} />;
-          })}
-        </div>
+        </Box>
+
+        <Box sx={{padding: 2}}>
+          <Masonry
+            columns={{
+              xs: 1, md: 2, lg:3, xl:4
+            }}
+            spacing={2}
+            defaultHeight={450}
+            defaultColumns={4}
+            defaultSpacing={2}
+          >
+            {ProjectsData.data.map((repo) => {
+              return (
+                  <Fade key={repo.id} bottom duration={2000} distance="40px">
+                    <ProjectCard repo={repo} theme={theme} />
+                  </Fade>
+              )
+            })}
+          </Masonry>
+        </Box>
+
+        {/*<div className="repo-cards-div-main">*/}
+        {/*  {ProjectsData.data.map((repo) => {*/}
+        {/*    return <GithubRepoCard repo={repo} theme={theme} />;*/}
+        {/*  })}*/}
+        {/*</div>*/}
+
         <Button
           text={"More Projects"}
           className="project-button"
