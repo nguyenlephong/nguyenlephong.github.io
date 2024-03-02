e7a02d206bbc3a779312897025e923cca01e9329
Phong Nguyen <nguyenlephong1997@gmail.com>
2024-03-02 08:25:20 +0700

    [init] - cv version 3.0

--- components/containers/tools/tool_002/types.js
+++
@@ -1,5 +0,0 @@
-export const SheetJSFT = [
-  "xlsx", "xlsb", "xlsm", "xls", "xml", "csv", "txt", "ods", "fods", "uos", "sylk", "dif", "dbf", "prn", "qpw", "123", "wb*", "wq*", "html", "htm"
-].map(function (x) {
-  return "." + x;
-}).join(",");
