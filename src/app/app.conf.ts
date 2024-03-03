13486c23d3641ec28e75f58d39b7286233be62b9
Phong Nguyen <nguyenlephong1997@gmail.com>
2024-03-03 17:39:12 +0700

    feat (page): add gallery page

---
+++ src/app/app.conf.ts
@@ -0,0 +1,1 @@
+export const CDN_PATH = "https://cdn.jsdelivr.net/gh/nguyenlephong/dom-pub/shared/images/cv"
--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -4,8 +4,16 @@ const PDFResumeViewer = dynamic(() => import("@/components/PDFResumeViewer"));
 
 export default function CVPage() {
   return (
-    <main style={{marginTop: 60}}>
-      <PDFResumeViewer />
+    <main className={"about-page"}>
+      <section className={"section-container"}>
+        <h1 style={{textAlign: "center", fontSize: 32, padding: 24}}>
+          Front-end Software Engineer
+        </h1>
+        
+        <div className={"section-wrapper"}>
+          <PDFResumeViewer />
+        </div>
+      </section>
     </main>
   );
 }
