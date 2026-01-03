e493d96b72a087e1c3e478764fe21bd83f4ac00c
Phong Nguyen <phongnguyen.itengineer@gmail.com>
2026-01-03 11:09:30 +0700

    feat: add cv file

--- src/components/PDFResumeViewer.tsx
+++ src/components/PDFResumeViewer.tsx
@@ -7,7 +7,8 @@ export default function PDFResumeViewer(){
     <div style={{textAlign: "center"}}>
       <div  style={{width: "100%", maxWidth: 1200, padding: 12, display: "inline-block"}}>
         <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js`}>
-          <Viewer fileUrl={`/Frontend_NguyenLePhong_0985490107.pdf`} initialPage={2} />
+          <Viewer fileUrl={`/SoftwareEngineer_NguyenLePhong_0985490107_NoRefs.pdf`} initialPage={2} />
+          {/*<Viewer fileUrl={`/Frontend_NguyenLePhong_0985490107.pdf`} initialPage={2} />*/}
         </Worker>
       </div>
     </div>
